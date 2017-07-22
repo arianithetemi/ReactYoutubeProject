b8358010e0b5be4804973785b109a62b238846cc
Arianit <nitihetemi@gmail.com>
2017-07-22 16:05:56 +0200

    Feeding videos from yt api and rendering as video list item in video list

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 // Libs
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 // Components
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = "AIzaSyCvyRhAluoUohlVYpcynC5XiInYq2LV4pg";
 
-// Create new component
+// Main component
 class App extends Component {
-  // Produce HTML
+  constructor(props){
+    super(props);
+
+    this.state = {videos: []}
+
+    YTSearch({key: API_KEY, term: 'reactjs'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   }
 }
-
 
 // Render HTML component in DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
